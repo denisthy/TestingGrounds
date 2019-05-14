31729c6fab6ee8a08e45e02360a88f91959335be
denisthy <lp.denisthy@gmail.com>
2019-05-14 09:58:08 +0200

    Added Patrolling Guard, Choose Next Way Point from BP to CPP

--- Source/TestingGrounds/Private/TP_ThirdPerson/ChooseNextWayPoint.cpp
+++ Source/TestingGrounds/Private/TP_ThirdPerson/ChooseNextWayPoint.cpp
@@ -3,12 +3,31 @@
 
 #include "ChooseNextWayPoint.h"
 #include "..\..\Public\TP_ThirdPerson\ChooseNextWayPoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
 #include "BehaviorTree/BlackboardComponent.h"
 EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	// Todo protect against empty patrol route
+
+
+	//Get Patrol Point
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->GetPatrolPoints();
+
+	//Set Next WayPoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp,Warning,TEXT("SUCCEEDED : %i"),Index);
-	
+	BlackboardComp->SetValueAsObject(WayPointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle the Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+
+
+	UE_LOG(LogTemp, Warning, TEXT("SUCCEEDED : %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
--- Source/TestingGrounds/Public/TP_ThirdPerson/ChooseNextWayPoint.h
+++ Source/TestingGrounds/Public/TP_ThirdPerson/ChooseNextWayPoint.h
@@ -20,4 +20,6 @@ class TESTINGGROUNDS_API UChooseNextWayPoint : public UBTTaskNode
 protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WayPointKey;
 };
